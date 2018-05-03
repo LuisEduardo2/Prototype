f81a8ebea95671b10dc10d22b944082ec0a8b74d
dudu <LuisEduardo>
2018-05-02 21:59:59 -0300

    Flask 1.0 updates

---
+++ Flask-Web/app.py
@@ -0,0 +1,8 @@
+from app import app
+
+
+'''
+#this runner still working, but it is no longer recommended for this and future flask versions
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080,debug=True)
+'''
